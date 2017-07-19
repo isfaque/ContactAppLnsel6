991d0aeb2f6a557f1ca5a55b3b3c4b489bd5a255
isfaque <isfaque25@gmail.com>
2017-07-19 14:02:45 +0530

    First commit

---
+++ app/src/main/java/apps/lnsel/com/contactapp/ContactsData.java
@@ -0,0 +1,10 @@
+package apps.lnsel.com.contactapp;
+
+import java.util.ArrayList;
+
+/**
+ * Created by apps2 on 7/17/2017.
+ */
+public class ContactsData {
+    public static ArrayList<ContactsSetterGetter> contactsList = new ArrayList<ContactsSetterGetter>();
+}
